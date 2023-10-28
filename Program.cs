ee6dd137a785bfb28fa6d611f308ab2a49910c96
isabellagoes <isabellagoes.igs@gmail.com>
2023-10-27 22:30:18 -0300

    Inserção de observação

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿int numeroDigitado = -1;
+﻿//OBS: No caso de um número fora do padrão ser inserido o programa continuará, o mesmo só encerra com um número correto ou 0
+
+Console.WriteLine("--- Entrada Válida ---\n");
+
+int numeroDigitado = -1;
 
 while (numeroDigitado < 0 || numeroDigitado > 9)
 {
     Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
     Int32.TryParse(Console.ReadLine()!, out numeroDigitado);
+    //OBS: No caso de um número fora do padrão ser inserido o programa continuará, o mesmo só encerra com um número correto ou 0
 }
 
 if (numeroDigitado == 0)
