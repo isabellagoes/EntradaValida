a33934616e6876fbff4075071226f598f462e02e
isabellagoes <isabellagoes.igs@gmail.com>
2023-10-27 22:27:27 -0300

    Validação de entrada de um número somente entre 1 a 9 (inclusive) e continuação do programa caso um número fora do padrão seja inserido. Cancelamento da operação no caso da entrada do número 0

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int numeroDigitado = -1;
+
+while (numeroDigitado < 0 || numeroDigitado > 9)
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    Int32.TryParse(Console.ReadLine()!, out numeroDigitado);
+}
+
+if (numeroDigitado == 0)
+{
+    Console.WriteLine("\nOperação cancelada.");
+}
+
+else
+{
+    Console.WriteLine($"\nNúmero selecionado = {numeroDigitado}");
+}
+
+Console.WriteLine("\nVolte sempre!");
